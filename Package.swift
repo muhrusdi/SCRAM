ec6175d012f4c804905e53f14a28545b84bbc063
Muh Rusdi <muhrusdi@Muhs-MacBook-Pro.local>
2016-07-27 18:01:42 +0800

    first commit

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SCRAM",
     dependencies: [
-        .Package(url: "https://github.com/CryptoKitten/PBKDF2.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/muhrusdi/PBKDF2.git", majorVersion: 0, minor: 9),
     ]
 )
